3f55229618fc30e6f1e3eea61e92e0efb093a63e
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2022-01-24 15:28:30 +0300

    Create MintablerNFT.sol

---
+++ MintablerNFT.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MintableNFT is ERC721Enumerable, ERC721URIStorage, AccessControl {
+    
+    mapping (string => uint256) public hashToId;
+
+    constructor() ERC721("Mintable NFT","MNFT") {
+        _safeMint(msg.sender, 0);
+        _setTokenURI(0, "https://i.imgur.com/ow57Hgj.jpeg");
+        hashToId["reserved"] = 0;
+    }
+
+    function mint(address owner, string calldata uniqueHash, string calldata mediaURL) external {
+        require (hashToId[uniqueHash] != 0, "MintableNFT: This hash is already used");
+        uint256 tokenId = totalSupply();
+        _safeMint(owner, tokenId);
+        _setTokenURI(tokenId, mediaURL);
+        hashToId[uniqueHash] = tokenId;
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return ERC721URIStorage.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable, AccessControl) returns (bool) {
+        return ERC721.supportsInterface(interfaceId) || ERC721Enumerable.supportsInterface(interfaceId) || AccessControl.supportsInterface(interfaceId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        ERC721URIStorage._burn(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        ERC721Enumerable._beforeTokenTransfer(from, to, tokenId);
+    }
+}
